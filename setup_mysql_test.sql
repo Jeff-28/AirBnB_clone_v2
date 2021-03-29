a81cdc173d3ed57bdfd629a9952bf091d1c257cc
Jeff Martinez <2408@holbertonschool.com>
2021-03-29 17:58:46 +0000

    changed the database for the GRANT SELECT

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -10,4 +10,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grants privileges
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
