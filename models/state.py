8e8914e1d9de1fe860344537295eb198a1bd3209
Lohkrii <joshualopez0429@gmail.com>
2021-03-30 14:47:24 +0000

    Adding revamped: base_mode, city, state and added a database storage inside engine folder

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Module of the State class """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ Implementation for the State """
+
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", passive_deletes=True, backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            '''Getter for cities'''
+            cities = [value for key, value in models.storage.all().items()
+                      if 'City' in key and value.state_id == self.id]
+            return cities
