a7969e72775779f3274b945836bbd0c0ee240621
Jeff Martinez <2408@holbertonschool.com>
2021-03-31 15:03:04 +0000

    Added init method and some details

--- models/user.py
+++ models/user.py
@@ -9,10 +9,20 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
 
-    reviews = relationship("Review", cascade="all, delete", backref="user")
-    places = relationship("Place", cascade="all, delete", backref="user")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        reviews = relationship("Review", cascade="all, delete", backref="user")
+        places = relationship("Place", cascade="all, delete", backref="user")
+    else:
+        first_name = ""
+        last_name = ""
+        password = ""
+        email = ""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a User object"""
+        super().__init__(*args, **kwargs)
