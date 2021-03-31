a82b943fac3c1a83bd671091072f4ae30090ada3
Lohkrii <joshualopez0429@gmail.com>
2021-03-31 16:24:58 +0000

    Added packages

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import uuid
+from datetime import datetime
+import models
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv, environ
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
 
-import os
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
