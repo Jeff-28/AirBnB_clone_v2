74cc6a49560416612854bc72dd1bf37660127fc8
Jeff Martinez <2408@holbertonschool.com>
2021-04-02 22:06:29 +0000

    fixed user_id attribute

--- models/place.py
+++ models/place.py
@@ -13,9 +13,9 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    if (getenv("HBNB_TYPE_STORAGE") == 'db'):
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -24,7 +24,6 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        amenity_ids = []
     else:
         city_id = ""
         user_id = ""
