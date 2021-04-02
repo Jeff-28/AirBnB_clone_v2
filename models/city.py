daa869eef80e85f0bcb99058a8df0a9950db9f2d
Jeff Martinez <2408@holbertonschool.com>
2021-04-02 21:36:39 +0000

    fixed getenv parameter

--- models/city.py
+++ models/city.py
@@ -13,10 +13,10 @@ class City(BaseModel, Base):
     """ Define the class City that inherits from BaseModel """
 
     __tablename__ = 'cities'
-    if getenv('HBNB_STORAGE_TYPE') == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", cascade="all, delete", backref="cities")
+        places = relationship("Place", backref="cities")
     else:
         name = ""
         state_id = ""
